88b33997c3babe4b6a7403767a22bf15f93ef9d4
18429FukadaKouta <86936732+18429FukadaKouta@users.noreply.github.com>
2021-11-17 15:07:43 +0900

    PEP8準拠

--- udp_s.py
+++ udp_s.py
@@ -1,8 +1,8 @@
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as A11:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as a11:
     A11.bind((' 127.0.0.1', 50007))
     while True:
-        RECVdata, fRoMaDdr = A11.recvfrom(1024)
-        print("data: {}, addr: {}".format(RECVdata, fRoMaDdr))
+        recv_data, from_addre = a11.recvfrom(1024)
+        print("data: {}, addr: {}".format(recv_data, from_addre))
         
